bcc328a9abe5cc6cb05297a6e609e0729d188cc0
Max Sorensen <medal4me2@gmail.com>
2016-05-06 15:51:25 +1000

    Initial commit.

---
+++ app.js
@@ -0,0 +1,91 @@
+var _ = require('lodash');
+var async = require('async');
+var config = require('config');
+var cron = require('cron');
+var fs = require('fs');
+var path = require('path');
+var Promise = require('bluebird');
+var request = require('request-promise');
+var winston = require('winston');
+var youtube = require('youtube-mp3-downloader');
+
+Promise.config({
+  warnings: false
+});
+
+function doWork() {
+  winston.info('Starting download job...');
+  return readPage()
+    .then(function(videos) {
+      return async.eachSeries(videos, function(video) {
+        var stats = fs.statSync(path.join(config.get('destination'), video.title + '.mp3'));
+        if (stats.isFile())
+          return;
+
+        winston.info('Downloading %s [%s]...', video.title, video.videoId);
+
+        return new Promise(function(resolve, reject) {
+          var YD = new youtube({
+            ffmpegPath: config.get('ffmpeg.location'),
+            outputPath: config.get('destination'),
+            youtubeVideoQuality: 'highest'
+          });
+
+          YD.on('finished', function(data) {
+            winston.info('Downloaded %s.', video.title);
+            return resolve(data);
+          });
+
+          YD.on('error', function(error) {
+            return reject(error);
+          });
+
+          YD.download(video.videoId);
+        });
+      });
+    })
+    .then(function() {
+      winston.info('Download job complete.');
+    })
+    .catch(function(error) {
+      winston.error('Download error', error.stack || error);
+    });
+}
+
+function readPage(pageToken) {
+  return request.get('https://www.googleapis.com/youtube/v3/playlistItems', {
+    json: true,
+    qs: {
+      key: config.get('youtube.apiKey'),
+      maxResults: 10,
+      pageToken: pageToken,
+      part: 'snippet',
+      playlistId: config.get('youtube.playlistId')
+    }
+  }).then(function(results) {
+    var videos = _.map(results.items, function(item) {
+      return {
+        title: _.get(item, 'snippet.title'),
+        videoId: _.get(item, 'snippet.resourceId.videoId')
+      };
+    });
+
+    if (!results.pageInfo.nextPageToken)
+      return videos;
+
+    return readPage(results.pageInfo.nextPageToken)
+      .then(function(more) {
+        return _.concat(videos, more);
+      });
+  });
+}
+
+var queue = async.queue(doWork, 1);
+var job = new cron.CronJob({
+  cronTime: config.get('cron.frequency'),
+  onTick: function() {
+    queue.push();
+  },
+  start: true,
+  runOnInit: true
+});
